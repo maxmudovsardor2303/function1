2c16b656c819f7e1152cad827879d6aff52fb0a5
Your Name <you@example.com>
2024-03-22 23:41:38 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,94 @@
+// 1-masala
+
+// let arr = [1,2,3,4,5,6,7]
+
+// let even = 0
+// let odd = 0
+//     for(let i = 1; i <= arr.length; i++){
+//         if(arr[i] % 2 ===0){
+//             even += 1
+//         }else{
+//             odd += 1
+//         }
+
+//     }
+// console.log(even);
+// console.log(odd);
+
+
+// 2-masala 
+
+// let arr = [1,2,3,4,5,6]
+
+// let even1 = 0
+// let odd2 = 0
+
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] % 2 === 0){
+//         even1 += arr[i]
+//     }else{
+//         odd2 += arr[i]
+//     }
+// }
+// console.log(even1-odd2);
+
+
+// 3-masala
+
+// function countArgement(...params) {
+//     let Argement = 0
+//     for(let arg of params){
+//         Argement += arg
+//     }
+//     console.log(Argement);
+// }
+// countArgement(1,2,3,4,5,6)
+
+
+// 4-masala
+
+
+// function getString(arr) {
+//     let  newArry =[]
+//     for(element of arr){
+//         if ((typeof element)==='number') {
+//             newArry.push(element);
+//         }
+//     }
+//     console.log(newArry);
+// }
+// getString([123,"Hello",true,{name:"sardor"},"Najot ta'lim",function name(params) {
+    
+// }])
+
+
+
+// 5- masala
+
+// let arr =[6,4,1]
+
+// function getNewArry(params) {
+//     if(arr[0]>params){
+//         arr.unshift(params)
+       
+//     }else{
+//         arr.push(params)
+//     }
+//     console.log(arr);
+// }
+// getNewArry(3)
+
+
+// 6- masala
+
+// function getString(arr) {
+//   let newArr=[]
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         newArr.push(arr[i])
+//     }
+//   } 
+//   console.log(newArr);
+// }
+// getString([1,2,3,4,5,6,7,8,])
+
